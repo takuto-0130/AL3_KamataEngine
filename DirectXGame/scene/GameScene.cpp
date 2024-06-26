9b183b33102b20ffd1ae37b2882aec548a75a9a7
takuto-0130 <k023g0005@g.neec.ac.jp>
2024-06-26 11:51:32 +0900

    temp

--- DirectXGame/scene/GameScene.cpp
+++ DirectXGame/scene/GameScene.cpp
@@ -4,16 +4,24 @@
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() {
+	delete model_;
+	delete player_;
+}
 
 void GameScene::Initialize() {
 
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
+	texHandle_ = TextureManager::Load("./Resources/cube/cube.jpg");
+	model_ = Model::Create();
+	viewProjection_.Initialize();
+	player_ = new Player();
+	player_->Initialize(model_, texHandle_);
 }
 
-void GameScene::Update() {}
+void GameScene::Update() { player_->Update(); }
 
 void GameScene::Draw() {
 
@@ -41,6 +49,8 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
+
+	player_->Draw(viewProjection_);
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
