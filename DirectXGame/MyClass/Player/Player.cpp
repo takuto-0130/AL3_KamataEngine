d4d9cb4de621132520255e99456209b0410c8cbf
takuto-0130 <k023g0005@g.neec.ac.jp>
2024-06-26 11:45:03 +0900

    temp

--- DirectXGame/MyClass/Player/Player.cpp
+++ DirectXGame/MyClass/Player/Player.cpp
@@ -1,1 +1,14 @@
 #include "Player.h"
+
+void Player::Initialize(Model* model, uint32_t texHandle) { 
+	model_ = model;
+	texHandle_ = texHandle;
+}
+
+void Player::Update() { 
+	worldTransform_.TransferMatrix();
+}
+
+void Player::Draw(ViewProjection& viewProjection) { 
+	model_->Draw(worldTransform_, viewProjection, texHandle_);
+}
--- DirectXGame/MyClass/Player/Player.h
+++ DirectXGame/MyClass/Player/Player.h
@@ -3,8 +3,32 @@
 #include "Input.h"
 #include "Model.h"
 #include "Sprite.h"
+#include "ViewProjection.h"
 #include "WorldTransform.h"
+#include <assert.h>
 
+/// <summary>
+/// 自キャラ
+/// </summary>
 class Player {
+public:
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize(Model* model, uint32_t texHandle);
 
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw(ViewProjection& viewProjection);
+
+private:
+	WorldTransform worldTransform_;
+	Model* model_ = nullptr;
+	uint32_t texHandle_ = 0;
 };
